80993a9f7382e2d0b65e484c72121bcd0fc46124
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-07-20 17:46:27 +0900

    자바 공부 15일차 - 클래스의 구성 멤버 및 필드 : 필드 선언 및 초기화, 접근

---
+++ ThisisJava Chapter6/sec06/exam01/Car.java
@@ -0,0 +1,24 @@
+package ch06.sec06.exam01;
+
+public class Car {
+	//필드 선언
+	String company = "현대자동차";
+	String model = "그랜저";
+	String color = "검정";
+	boolean start = false;
+	int maxSpeed = 350;
+	int speed;
+	
+	Car(){
+	
+	}
+	
+	static class Tire{
+		
+	}
+	
+	void setSpeed() {
+		
+	}
+	
+}
