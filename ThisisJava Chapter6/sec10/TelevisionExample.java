34b5f6b3b39b81107a7f9cfe976e95f94280680e
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-07-26 18:31:32 +0900

    자바 공부 18일차(20230726) - 정적 멤버
    static 키워드의 작동 방식 및  정적 멤버와 블록의 선언 및 사용법, 인스턴스 멤버와의 차이점과 상호 사용 방법에 대한 정리.

---
+++ ThisisJava Chapter6/sec10/TelevisionExample.java
@@ -0,0 +1,10 @@
+package ch06.sec10;
+
+public class TelevisionExample {
+
+	public static void main(String[] args) {
+		//아래는 정적 멤버 선언 후, 개체 생성 없이 멤버를 출력하는 예제.
+		System.out.println(Television.info);
+	}
+
+}
