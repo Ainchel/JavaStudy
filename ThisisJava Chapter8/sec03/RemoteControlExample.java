10fea19c5f8568d2af9fb5a947fcda9d7ab1aef0
Ainchel <134568940+Ainchel@users.noreply.github.com>
2023-08-14 19:16:41 +0900

    자바 공부 29일차 - 인터페이스의 상수 필드
    상수 필드 선언 및 활용에 대한 예제.

---
+++ ThisisJava Chapter8/sec03/RemoteControlExample.java
@@ -0,0 +1,12 @@
+package ch08.sec03;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+
+		//인터페이스의 상수 필ㄷ 사용해보기(구현도 필요없음.)
+		
+		System.out.println("리모콘 최대 볼륨 : " + RemoteControl.MAX_VOLUME);
+		System.out.println("리모콘 최소 볼륨 : " + RemoteControl.MIN_VOLUME);
+	}
+}
